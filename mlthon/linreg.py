97f383ef57ba7d3fef014662d7c72a1ecb555ed4
pritishmishra703 <pritishjan@gmail.com>
2021-05-08 20:49:52 +0530

    Changed folder structure

--- mlthon/linreg.py
+++ mlthon/linreg.py
@@ -39,12 +39,14 @@ class RidgeRegression() :
             self.W -= self.learning_rate*LW
             self.b -= self.learning_rate*Lb
 
+            # self.learning_rate = self.learning_rate/
+
             if self.get_log == True:
                 if i%10 == 0:
                     self.y_pred_log.append(y_pred)
                 self.loss_log.append(np.mean(np.square(y - y_pred)))
 
-      
+
     def predict(self, X):
         return np.dot(X, self.W) + self.b
 
@@ -137,3 +139,37 @@ class ElasticNet() :
       
     def predict(self, X) :    
         return np.dot(X, self.W) + self.b
+
+
+class LogisticRegression:
+
+    def __init__(self, penalty='l2', tol=1e-4, C=1.0, fit_intercept=True, max_iter=10000, 
+    l1_ratio=None):
+        self.penalty = penalty
+        self.tol = tol
+        self.C = C
+        self.fit_intercept = fit_intercept
+        self.max_iter = max_iter
+        self.l1_ratio = l1_ratio
+
+    def fit(self, X, y):
+        self.W = np.zeros(X.shape[1])
+        self.b = 0
+        m = X.shape[0]
+        learning_rate = 0.0001
+
+        for i in range(self.max_iter):
+            y_pred  = self.predict(X)
+            error = y - y_pred
+            Lw = X.T.dot(error)/m
+            Lb = np.sum(error)/m
+
+            self.W -= (learning_rate*Lw + (2*1.0*self.W))/X.shape[0]
+            self.b -= learning_rate*Lb
+
+    def predict(self, X):
+        predictions = np.dot(X, self.W) + self.b
+        return self.sigmoid(predictions)
+
+    def sigmoid(self, X):
+        return 1/(1 + np.exp(-X))
