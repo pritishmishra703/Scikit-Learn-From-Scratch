d73d7db786615a077a64a2f04252efef5bf02a8b
pritishmishra703 <pritishjan@gmail.com>
2021-05-14 20:00:09 +0530

    Their are many changes I can't figure which to write!

--- mlthon/linreg.py
+++ mlthon/linreg.py
@@ -39,8 +39,6 @@ class RidgeRegression() :
             self.W -= self.learning_rate*LW
             self.b -= self.learning_rate*Lb
 
-            # self.learning_rate = self.learning_rate/
-
             if self.get_log == True:
                 if i%10 == 0:
                     self.y_pred_log.append(y_pred)
@@ -143,33 +141,29 @@ class ElasticNet() :
 
 class LogisticRegression:
 
-    def __init__(self, penalty='l2', tol=1e-4, C=1.0, fit_intercept=True, max_iter=10000, 
+    def __init__(self, penalty='l2', learning_rate=0.001, C=1.0, fit_intercept=True, max_iter=5000, 
     l1_ratio=None):
         self.penalty = penalty
-        self.tol = tol
         self.C = C
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
         self.l1_ratio = l1_ratio
+        self.learning_rate = learning_rate
 
     def fit(self, X, y):
-        self.W = np.zeros(X.shape[1])
-        self.b = 0
-        m = X.shape[0]
-        learning_rate = 0.0001
+        n_features = X.shape[1]
+        limit = 1/np.sqrt(n_features)
+        self.W = np.random.uniform(-limit, limit, (n_features,))
 
         for i in range(self.max_iter):
             y_pred  = self.predict(X)
-            error = y - y_pred
-            Lw = X.T.dot(error)/m
-            Lb = np.sum(error)/m
+            Lw = -(y - y_pred).dot(X)
 
-            self.W -= (learning_rate*Lw + (2*1.0*self.W))/X.shape[0]
-            self.b -= learning_rate*Lb
+            self.W -= self.learning_rate * Lw
 
     def predict(self, X):
-        predictions = np.dot(X, self.W) + self.b
-        return self.sigmoid(predictions)
+        predictions = self.sigmoid(X.dot(self.W))
+        return predictions
 
     def sigmoid(self, X):
         return 1/(1 + np.exp(-X))
