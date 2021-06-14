9b74695d3d174543718c9a98325496072d8f43ab
Pritish Mishra <pritishjan@gmail.com>
2021-06-14 16:22:25 +0530

    Created neighbours.py

--- mlthon/linreg.py
+++ mlthon/linreg.py
@@ -141,30 +141,44 @@ class ElasticNet() :
 
 class LogisticRegression:
 
-    def __init__(self, penalty='l2', learning_rate=0.001, C=1.0, fit_intercept=True, max_iter=5000, 
-    l1_ratio=None):
+    def __init__(self, penalty='l2', learning_rate=0.0001, C=1.0, fit_intercept=True, max_iter=5000, 
+    l1_ratio=None, print_info=False):
         self.penalty = penalty
         self.C = C
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
         self.l1_ratio = l1_ratio
         self.learning_rate = learning_rate
+        self.print_info = print_info
 
     def fit(self, X, y):
-        ones = np.ones(len(X)).reshape(-1, 1)
-        X = np.concatenate((ones, X), axis=1)
-        
-        self.W = np.zeros(X.shape[1])
+        self.weights = np.random.rand(X.shape[1])
+        self.bias = 0
+        self.m = len(X)
 
         for i in range(self.max_iter):
             y_pred = self.predict(X)
-            Lw = np.dot(X.T, y_pred - y)
+            Lw = np.dot(X.T, (y_pred - y)) / self.m
+            Lb = np.sum(y_pred - y) / self.m
+            self.weights = self.weights - self.learning_rate * Lw
+            # self.bias = self.bias - self.learning_rate * Lb
 
-            self.W -= self.learning_rate * Lw
+            if self.print_info == True:
+                if (i + 1)%100 == 0:
+                    loss = self.BinaryCrossEntropy(y, y_pred)
+                    print(f"Iteration: {i+1}, Loss: {loss}")
 
+    def BinaryCrossEntropy(self, y_true, y_pred):
+        y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        term_0 = (1 - y_true) * sum(np.log(1 - y_pred + 1e-7))
+        term_1 = y_true * sum(np.log(y_pred + 1e-7))
+        loss = -1.0/self.m * np.mean(term_0+term_1, axis=0)
+        return loss
+    
     def predict(self, X):
-        predictions = self.sigmoid(X.dot(self.W))
+        predictions = self.sigmoid(np.dot(X, self.weights)).astype('float32')
+        predictions = np.where(predictions >= 0.5, 1, 0)
         return predictions
 
     def sigmoid(self, X):
-        return 1/(1 + np.exp(-X))
+        return 1/(1 + np.e**(-X))
